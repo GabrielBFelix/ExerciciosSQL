110d39e1bcdfe22f6111546974fcb95fe0faf842
GabrielBFelix <batistagabriel360@gmail.com>
2020-11-09 17:45:53 -0300

    Adicionando exercício 2

---
+++ Tarefa 05 - Funções e Procedimentos/2.sql
@@ -0,0 +1,13 @@
+-- 2. Faça uma consulta para calcular a média de idade por departamento.
+
+CREATE OR REPLACE FUNCTION media_idade_por_dep(INTEGER) RETURNS TABLE (idade INTERVAL) LANGUAGE 'plpgsql' AS $$
+BEGIN
+	RETURN QUERY(
+		SELECT AVG(AGE(f.datanasc)) AS idade
+		FROM funcionario f
+		WHERE f.depto = $1
+	);
+END;
+$$;
+
+SELECT media_idade_por_dep(1);
